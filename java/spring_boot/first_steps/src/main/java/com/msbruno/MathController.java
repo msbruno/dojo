1276d8135fda8532334d90566d6edd33d8502486
silva <ms.bruno1@gmail.com>
2023-06-02 15:42:02 -0300

    feat: change params to String to be able to check them

--- java/spring_boot/first_steps/src/main/java/com/msbruno/MathController.java
+++ java/spring_boot/first_steps/src/main/java/com/msbruno/MathController.java
@@ -1,5 +1,6 @@
 package com.msbruno;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +13,29 @@ public class MathController {
 			value = "/sum/{param1}/{param2}",
 			method=RequestMethod.GET)
 	public Double sum(
-			@PathVariable(name = "param1") Double param1,
-			@PathVariable(name = "param2") Double param2) {
-		return param1 + param2;
+			@PathVariable(name = "param1") String param1,
+			@PathVariable(name = "param2") String param2) {
+		
+		param1 = treatParameter(param1);
+		param2 = treatParameter(param2);
+		
+		if (!AreNumeric(param1, param2)) {
+			return Double.MAX_VALUE;
+		}
+		
+		return Double.valueOf(param1) + Double.valueOf(param2);
+	}
+
+	private String treatParameter(String strNumber) {
+		return strNumber.replace(',', '.');
+	}
+
+	private boolean AreNumeric(String... paramList) {
+		for (String param : paramList) {
+			if (!StringUtils.isNumeric(param)){
+				return false;
+			}
+		}
+		return true;
 	}
 }
