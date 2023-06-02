768c7070377cb6f9922e5f5f9cbfbfcf670fb044
silva <ms.bruno1@gmail.com>
2023-06-02 14:46:58 -0300

    feat: add message and id generator

--- java/spring_boot/first_steps/src/main/java/com/msbruno/GreetingController.java
+++ java/spring_boot/first_steps/src/main/java/com/msbruno/GreetingController.java
@@ -1,15 +1,21 @@
 package com.msbruno;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+	
+	private String message = "Hello, %s";
+	private AtomicLong idGenerator = new AtomicLong();
 
 	@RequestMapping("/greet")
 	public Greeting greeting(
 			@RequestParam(name="name", defaultValue = "World") String name) {
-		return new Greeting(0, name);
+		String messageResult = String.format(message, name);
+		return new Greeting(idGenerator.getAndIncrement(), messageResult);
 	}
 }
