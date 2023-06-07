9738c03a7a0fe18208543689fd1e6c96794e10bd
silva <ms.bruno1@gmail.com>
2023-06-07 17:09:08 -0300

    feat: add repository to Person

--- java/spring_boot/first_steps/src/main/java/com/msbruno/entities/Person.java
+++ java/spring_boot/first_steps/src/main/java/com/msbruno/entities/Person.java
@@ -2,10 +2,24 @@ package com.msbruno.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public record Person (
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		Long id, 
-		String firstName, 
-		String surname, 
+		@Column(name = "name", length = 80)
+		String firstName,
+		@Column(name = "surname", length = 80)
+		String surname,
+		@Column(name = "phone", length = 80)
 		String phone) implements Serializable {
 	
 }
---
+++ java/spring_boot/first_steps/src/main/java/com/msbruno/entities/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.msbruno.entities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+}
